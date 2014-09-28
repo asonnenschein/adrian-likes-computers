7a7e9ae1d8e40e08a07842619a288ebdd2f3ded5
asonnenschein <adrian.sonnenschein@gmail.com>
2014-09-28 02:36:30 -0700

    nunjucks templating system

--- server.js
+++ server.js
@@ -1,29 +1,41 @@
 var express = require('express')
+  , nunjucks = require('nunjucks')
   , fs = require('fs')
-  , path = require('path');
+  , path = require('path')
+  ;
 
 var server = express();
 
-var style = path.join(__dirname, 'client', 'style')
-  , script = path.join(__dirname, 'client', 'script')
-  , vendor = path.join(__dirname, 'client', 'vendor')
-  , http = path.join(__dirname, 'client', 'serverhowto.html')
-  , home = path.join(__dirname, 'client', 'index.html');
+var images = path.join(__dirname, 'images')
+  , style = path.join(__dirname, 'style')
+  , script = path.join(__dirname, 'script')
+  , vendor = path.join(__dirname, 'vendor')
+  ;
+
+nunjucks.configure('views', {
+  autoescape: true,
+  express: server
+});
 
 server.use('/style', express.static(style));
 server.use('/scripts', express.static(script));
+server.use('/images', express.static(images));
 server.use('/vendor', express.static(vendor));
 
-server.get('/', function (req, res) {
-  fs.readFile(home, 'utf8', function (err, text) {
-  	res.send(text);
-  })
+server.get('/thoughts', function (req, res) {
+  res.render('thoughts.html');
 });
 
-server.get('/node-js-core-http-server', function (req, res) {
-  fs.readFile(http, 'utf8', function (err, text) {
-  	res.send(text);
-  })
+server.get('/work', function (req, res) {
+  res.render('work.html');
+});
+
+server.get('/play', function (req, res) {
+  res.render('play.html');
+});
+
+server.get('/about', function (req, res) {
+  res.render('about.html');
 });
 
 server.listen(3000, function () {
