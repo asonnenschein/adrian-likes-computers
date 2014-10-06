a63c60ca2d10f7b3fe76301ec4a26e94d7d4c184
asonnenschein <adrian.sonnenschein@gmail.com>
2014-10-05 22:31:12 -0700

    style leaflet w/css tutorial

--- server.js
+++ server.js
@@ -10,6 +10,7 @@ var images = path.join(__dirname, 'images')
   , style = path.join(__dirname, 'style')
   , script = path.join(__dirname, 'script')
   , vendor = path.join(__dirname, 'vendor')
+  , fonts = path.join(__dirname, 'fonts')
   ;
 
 nunjucks.configure('views', {
@@ -21,6 +22,7 @@ server.use('/style', express.static(style));
 server.use('/scripts', express.static(script));
 server.use('/images', express.static(images));
 server.use('/vendor', express.static(vendor));
+server.use('/fonts', express.static(fonts));
 
 server.get('/thoughts', function (req, res) {
   res.render('thoughts.html');
